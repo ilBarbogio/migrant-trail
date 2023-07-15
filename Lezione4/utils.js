1e082ecf0212add887ea5ec2aaf8de973e306f9e
Carminati Alberto <barbogioster@gmail.com>
2023-07-15 09:34:44 +0200

    Better jsdocs

--- Lezione4/utils.js
+++ Lezione4/utils.js
@@ -1,6 +1,6 @@
 /**
  * Recupera il primo elmento con la classe specificata, ritorna l'elemento
- * @param {*} classe stringa
+ * @param {string} classe stringa
  * @returns 
  */
 export function elementoPerClasse(classe){
@@ -8,7 +8,7 @@ export function elementoPerClasse(classe){
 }
 /**
  * Recupera l'elemento della pagina con l'id specificata, ritorna l'elemento
- * @param {*} id stringa
+ * @param {string} id stringa
  * @returns 
  */
 export function elementoPerId(id){
@@ -17,24 +17,24 @@ export function elementoPerId(id){
 
 /**
  * Aggiunge una classe ad un elemento
- * @param {*} elemento elemento al quale aggiungere
- * @param {*} classe classe da aggiungere, stringa
+ * @param {HTMLElement} elemento elemento al quale aggiungere
+ * @param {string} classe classe da aggiungere, stringa
  */
 export function aggiungiClasse(elemento,classe){
 	elemento.classList.add(classe)
 }
 /**
  * Toglie una classe da un elemento
- * @param {*} elemento elemento al quale togliere
- * @param {*} classe classe da togliere, stringa
+ * @param {HTMLElement} elemento elemento al quale togliere
+ * @param {string} classe classe da togliere, stringa
  */
 export function togliClasse(elemento,classe){
 	elemento.classList.remove(classe)
 }
 /**
  * Verifica se un elemento ha una classe, ritorna un booleano
- * @param {*} elemento elemento sul quale verificare
- * @param {*} classe classe da cercare
+ * @param {HTMLElement} elemento elemento sul quale verificare
+ * @param {string} classe classe da cercare
  */
 export function haClasse(elemento,classe){
 	return elemento.classList.contains(classe)
@@ -42,8 +42,8 @@ export function haClasse(elemento,classe){
 
 /**
  * Modifica il valore di una classe css
- * @param {*} nome nome della variabile, stringa
- * @param {*} valore valore della variabile, stringa (o numero)
+ * @param {string} nome nome della variabile, stringa
+ * @param {string | number} valore valore della variabile, stringa (o numero)
  */
 export function impostaVariabileCss(nome,valore){
 	document.body.style.setProperty(nome,valore)
@@ -51,8 +51,8 @@ export function impostaVariabileCss(nome,valore){
 
 /**
  * Attiva un "listener" per i click del mouse ed esegue una funzione
- * @param {*} elemento elemento sul quale "ascoltare" i click
- * @param {*} handler funzione da eseguire, riceve due parametri: l'elemento e l'evento
+ * @param {HTMLElement} elemento elemento sul quale "ascoltare" i click
+ * @param {function} handler funzione da eseguire, riceve due parametri: l'elemento e l'evento
  */
 export function ascoltaClick(elemento,handler){
 	elemento.addEventListener("click",(ev)=>{
@@ -62,8 +62,8 @@ export function ascoltaClick(elemento,handler){
 
 /**
  * Attiva un "listener" per l'inizio di un'animazione ed esegue una funzione
- * @param {*} elemento elemento sul quale "ascoltare" l'inizio animazione
- * @param {*} handler funzione da eseguire, riceve due parametri: il nome dell'animazione e l'evento
+ * @param {HTMLElement} elemento elemento sul quale "ascoltare" l'inizio animazione
+ * @param {function} handler funzione da eseguire, riceve due parametri: il nome dell'animazione e l'evento
  */
 export function ascoltaInizioAnimazione(elemento,handler){
 	elemento.addEventListener("animationstart",(ev)=>{
@@ -73,8 +73,8 @@ export function ascoltaInizioAnimazione(elemento,handler){
 
 /**
  * Attiva un "listener" per la fine di un'animazione ed esegue una funzione
- * @param {*} elemento elemento sul quale "ascoltare" la fine animazione
- * @param {*} handler funzione da eseguire, riceve due parametri: il nome dell'animazione e l'evento
+ * @param {HTMLElement} elemento elemento sul quale "ascoltare" la fine animazione
+ * @param {function} handler funzione da eseguire, riceve due parametri: il nome dell'animazione e l'evento
  */
 export function ascoltaFineAnimazione(elemento,handler){
 	elemento.addEventListener("animationend",(ev)=>{
